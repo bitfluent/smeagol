4fbf7c8ad46a181e6874aae7de727d1c8a9dcbaa
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2011-01-04 09:47:55 +0800

    Add .ackrc to recognize haml, sass, scss files

--- cookbooks/homebrew/recipes/misc.rb
+++ cookbooks/homebrew/recipes/misc.rb
@@ -11,3 +11,10 @@ require root + '/providers/homebrew'
 %w(tig ack coreutils imagemagick sqlite gist wget hub fortune proctools markdown ctags-exuberant).each do |pkg|
   homebrew pkg
 end
+
+template "#{ENV['HOME']}/.ackrc" do
+  mode   0700
+  owner  ENV['USER']
+  group  Etc.getgrgid(Process.gid).name
+  source "dot.ackrc.erb"
+end
