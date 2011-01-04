d96a286e9706036d497daa164c56f0b112cc7a34
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2011-01-04 11:16:03 +0800

    WIP PS1

--- cookbooks/git/recipes/completion.rb
+++ cookbooks/git/recipes/completion.rb
@@ -33,3 +33,20 @@ execute "setup bash-completion profile sourcing in ~/.profile" do
   command "echo 'source `#{HOMEBREW} --prefix`/etc/bash_completion' >> ~/.profile"
   not_if  "grep -q 'bash_completion' ~/.profile"
 end
+
+
+# Set up PS1
+%w(bash_profile bashrc zshrc).each do |config_file|
+  execute "configure git-aware PS1 into defaults for ~/.#{config_file}" do
+    # PS1='\h:\W \u $(__git_ps1 " (%s)")\$ '
+    command %Q{if [ -f ~/.#{config_file} ]; then echo 'PS1=\'[\\u@\h \W\$(__git_ps1 " (%s)")]\$ \'' >> ~/.#{config_file}; fi}
+    #not_if  "grep -q 'PS1' ~/.#{config_file}"
+  end
+end
+
+execute "setup bash-completion profile sourcing in ~/.profile" do
+   # PS1='\h:\W \u $(__git_ps1 " (%s)")\$ '
+  command %Q{echo 'PS1=\'[\\u@\h \W\$(__git_ps1 " (%s)")]\$ \'' >> ~/.profile}
+  #not_if  "grep -q 'PS1' ~/.profile"
+end
+
